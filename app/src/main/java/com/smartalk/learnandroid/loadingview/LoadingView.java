ef0d6a198148a582e2b9c165621fe26a0258d832
潘磊 <panlei106@gmail.com>
2016-03-17 22:54:25 +0800

    add LoadingView

---
+++ app/src/main/java/com/smartalk/learnandroid/loadingview/LoadingView.java
@@ -0,0 +1,24 @@
+package com.smartalk.learnandroid.loadingview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by panl on 16/3/17.
+ * contact panlei106@gmail.com
+ */
+public class LoadingView extends View {
+    public LoadingView(Context context) {
+        super(context);
+    }
+
+    public LoadingView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public LoadingView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+    
+}
